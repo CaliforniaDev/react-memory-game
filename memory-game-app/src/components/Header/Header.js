5c38dccf9225914a9e575733f0b5b7e82db4e29c
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-02-15 23:28:36 -0800

    Create header component

---
+++ memory-game-app/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <h1>Animal Memory Game</h1>
+    </HeaderWrapper>
+  );
+}
+export default Header;
+
+const HeaderWrapper = styled.div`
+  height: 8rem;
+  background: gray;
+`
