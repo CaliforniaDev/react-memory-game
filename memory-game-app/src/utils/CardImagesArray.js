4771274d13e26f8c0187adc92648beb7ab27004e
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-02-18 20:20:58 -0800

    Extract images from directory

--- memory-game-app/src/utils/CardImagesArray.js
+++ memory-game-app/src/utils/CardImagesArray.js
@@ -1,3 +1,27 @@
-const CardImagesArray = [
+import { v4 as uuidv4 } from 'uuid';
 
-]
+
+
+const importImagesToObject = (r) => {
+  const images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+const imagesObj = importImagesToObject(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+
+const seperateObject = obj => {
+  const imagesArray = [];
+  const keys = Object.keys(obj);
+  keys.forEach(key => {
+    imagesArray.push({
+      key: obj[key],
+      id: uuidv4()
+    });
+  })
+  return imagesArray;
+}
+
+const Images = seperateObject(imagesObj);
+
+export default Images
+
