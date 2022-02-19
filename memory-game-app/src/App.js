954aa7856b430cf95267608a882f14440674a825
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-02-18 20:21:41 -0800

    Test image extracting function call

--- memory-game-app/src/App.js
+++ memory-game-app/src/App.js
@@ -1,14 +1,28 @@
 import Gameboard from "./components/Gameboard";
 import Header from "./components/Header/Header";
+import Images from "./utils/CardImagesArray";
+import styled from "styled-components";
 
 function App() {
+  console.log(Images[0].key)
   return (
     <>
     <Header />
     <Gameboard />
+    <ImageDiv />
     </>
       
   );
 }
+
+const ImageDiv = styled.div`
+background-image: url(${Images[4].key});
+background-repeat: no-repeat;
+height: 200px;
+background-color: red ;
+
+
+`
+
 
 export default App;
