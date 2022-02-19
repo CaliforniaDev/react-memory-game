226148686756a077d5b0ea2cab92fdda37d599a9
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-02-19 05:18:52 -0800

    Style card item

--- memory-game-app/src/components/Card.js
+++ memory-game-app/src/components/Card.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
-const Card = () => {
+import Images from '../utils/CardImagesArray';
+const Card = ({title, imageSrc}) => {
+  console.log(Images);
   return (
     <CardContainer>
+      <ImageWrapper>
+        <img src={Images[0].src} alt="Beaver" />
+      </ImageWrapper>
+      <h4>{Images[0].name}</h4>
     </CardContainer>
   );
 }
@@ -9,9 +15,44 @@ const Card = () => {
 export default Card;
 
 const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   flex: 0 0 32rem;
+  align-items: center;
   height: 40rem;
-  border-radius: 1.6rem;
+  border-radius: 0.8rem;
   color: #fff;
-  background: #fff;
+  background: rgba(204, 76, 38, 1);
+  overflow: hidden;
 `
+const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1.6rem;
+  background-color: rgba(242, 80, 38, 1);
+  border-radius: 0.8rem;
+  height: 28rem;
+  width: 28rem;
+  margin: 2.4rem;
+`
+// /* Color Theme Swatches in Hex */
+// .Wuling---Year-of-the-Ox-1-hex { color: #038C3E; }
+// .Wuling---Year-of-the-Ox-2-hex { color: #04BF45; }
+// .Wuling---Year-of-the-Ox-3-hex { color: #F2B807; }
+// .Wuling---Year-of-the-Ox-4-hex { color: #F29727; }
+// .Wuling---Year-of-the-Ox-5-hex { color: #F24C27; }
+
+// /* Color Theme Swatches in RGBA */
+// .Wuling---Year-of-the-Ox-1-rgba { color: rgba(2, 140, 62, 1); }
+// .Wuling---Year-of-the-Ox-2-rgba { color: rgba(3, 191, 69, 1); }
+// .Wuling---Year-of-the-Ox-3-rgba { color: rgba(242, 183, 7, 1); }
+// .Wuling---Year-of-the-Ox-4-rgba { color: rgba(242, 150, 38, 1); }
+// .Wuling---Year-of-the-Ox-5-rgba { color: rgba(242, 76, 38, 1); }
+
+// /* Color Theme Swatches in HSLA */
+// .Wuling---Year-of-the-Ox-1-hsla { color: hsla(146, 96, 28, 1); }
+// .Wuling---Year-of-the-Ox-2-hsla { color: hsla(140, 96, 38, 1); }
+// .Wuling---Year-of-the-Ox-3-hsla { color: hsla(45, 94, 48, 1); }
+// .Wuling---Year-of-the-Ox-4-hsla { color: hsla(33, 88, 55, 1); }
+// .Wuling---Year-of-the-Ox-5-hsla { color: hsla(11, 88, 55, 1); }
