1673b231d8306af4648d682da683628d4f216eb9
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-02-20 05:17:04 -0800

    Refactor code into individual functions

--- memory-game-app/src/utils/CardImagesArray.js
+++ memory-game-app/src/utils/CardImagesArray.js
@@ -1,21 +1,30 @@
 import { v4 as uuidv4 } from 'uuid';
 
 
-// import Images Object copied from stackoverflow
-const importImagesToObject = (r) => {
+function importImagesToObject(r) {
+  // EXTRACTS IMAGES FROM FOLDER INTO AN OBJECT //
   const images = {};
   r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
   return images;
 }
-const imagesObj = importImagesToObject(require.context('./images', false, /\.(png|jpe?g|svg)$/));
 
-const seperateObject = obj => {
+function convertImageSrcToTitle(imageSrc) {
+  //  CREATES OUR FILE NAME INTO A HEADER/TITLE //
+  const matchFileExtensions = /\.[^/.]+$/; // <-- THIS HELPS REMOVE FILE EXTENSIONS //
+  const matchCapitalLetters = /([A-Z])([A-Z])/; // <-- THIS HELPS ADD SPACING BETWEEN CAPITAL LETTERS //
+  imageSrc.replace(matchFileExtensions, "");
+  console.log(imageSrc);
+  return imageSrc;
+}
+
+function seperateObject (obj, convertImageSrcToTitle) {
+  // TAKE THE EXTRACTED FOLDER AND CREATE KEY VALUE PROPERTIES //
   const imagesArray = [];
-  const regexFileExtenstion = /\.[^/.]+$/;
   const keys = Object.keys(obj);
   keys.forEach(key => {
+    const imageTitle = convertImageSrcToTitle(key);
     imagesArray.push({
-      name: key.replace(regexFileExtenstion, ""), //<-- Removes File Extension//
+      name: imageTitle, 
       src: obj[key],
       id: uuidv4()
     });
@@ -23,7 +32,8 @@ const seperateObject = obj => {
   return imagesArray;
 }
 
-const Images = seperateObject(imagesObj);
+const imagesObj = importImagesToObject(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+const Images = seperateObject(imagesObj, convertImageSrcToTitle);
 
 export default Images
 
