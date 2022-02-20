e20745ee761dabb08d1020d3ee46d623dfaaeb62
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-02-20 05:16:12 -0800

    Add a grid mapping each card item onto gameboard

--- memory-game-app/src/components/CardGrid.js
+++ memory-game-app/src/components/CardGrid.js
@@ -1,1 +1,28 @@
 import CardItem from './CardItem';
+import Images from '../utils/CardImagesArray';
+import styled from 'styled-components';
+
+const CardGrid = () => {
+  const cards = Images.map(image => {
+    return (
+      <CardItem
+        imageSrc={image.src}
+        title={image.name}
+        key={image.id}
+      />
+    );
+  })
+  return (
+    <GridWrapper>
+      {cards}
+    </GridWrapper>
+  );
+}
+
+export default CardGrid;
+
+const GridWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 3.2rem;
+`
--- memory-game-app/src/components/Gameboard.js
+++ memory-game-app/src/components/Gameboard.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import CardGrid from './CardGrid';
 
 const Gameboard = () => {
   const [level, setLevel] = useState(1);
   
   return (
     <MainContainer>
+      <CardGrid />
     </MainContainer>
   );
 }
@@ -14,9 +16,10 @@ export default Gameboard;
 
 const MainContainer = styled.main`
   display: flex;
+  justify-content: center;
   padding: 2.4rem;
   flex-wrap: wrap;
   background: #403B31;
   width: 100vw;
-  height: 100vh;
+  height: 100%;
 `
