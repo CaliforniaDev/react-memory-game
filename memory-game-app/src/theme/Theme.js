c2fc610e3fbb384c9e6f505966eeb4a187004dec
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-02-20 04:14:08 -0800

    Change theme color swatches

---
+++ memory-game-app/src/theme/Theme.js
@@ -0,0 +1,11 @@
+const Theme = {
+  colors:{
+    green: '#3B8C47',
+    bgGreen: '#285E30',
+    yellow: '#C4B072',
+    bgYellow: '#5E5437',
+    orange: '#F28705',
+    bgOrange: '#C46E04',
+
+  }
+}
