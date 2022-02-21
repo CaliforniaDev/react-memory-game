67dd015a9cfa303ba8839dde9441ad1867877d31
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-02-20 23:48:31 -0800

    Refactor theme

--- memory-game-app/src/theme/theme.js
+++ memory-game-app/src/theme/theme.js
@@ -1,12 +1,13 @@
 const theme = {
   colors:{
     green: '#3B8C47',
-    bgGreen: '#285E30',
     yellow: '#C4B072',
-    bgYellow: '#5E5437',
     orange: '#F28705',
-    bgOrange: '#C46E04',
-
+  },
+  bg: {
+    green: '#285E30',
+    yellow: '#5E5437',
+    orange: '#C46E04',
   }
 }
 export default theme;
