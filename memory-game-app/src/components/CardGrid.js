2b1e71f999ca03a835dcf685a9cd477321d531d6
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-02-25 00:40:52 -0800

    Style grid wrapper

--- memory-game-app/src/components/CardGrid.js
+++ memory-game-app/src/components/CardGrid.js
@@ -4,6 +4,7 @@ import useSetCards from '../hooks/useSetCardsObject';
 
 const CardGrid = () => {
   const cards = useSetCards(Images);
+
   return (
     <GridWrapper>
       {cards}
@@ -15,6 +16,7 @@ export default CardGrid;
 
 const GridWrapper = styled.div`
   display: flex;
+  justify-content: center;
   flex-wrap: wrap;
   gap: 3.2rem;
 `
