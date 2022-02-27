1b6c24da4fbc708656ef64c69564cd7af0f995f3
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-02-26 19:30:49 -0800

    Fex card item render method

--- memory-game-app/src/components/CardGrid.js
+++ memory-game-app/src/components/CardGrid.js
@@ -1,22 +1,55 @@
 import Images from '../utils/CardImagesArray';
 import styled from 'styled-components';
-import useSetCards from '../hooks/useSetCardsObject';
+import CardItem from './CardItem';
+import theme from '../theme/theme'
 
 const CardGrid = () => {
-  const cards = useSetCards(Images);
+
+
+
+  const foregroundColors = Object.values(theme.fg);
+  const backgroundColors = Object.values(theme.bg);
+  const arraylength = backgroundColors.length - 1;
+  let index = 0;
 
   return (
-    <GridWrapper>
-      {cards}
+    <GridWrapper  >
+        {Images.map(image => {
+          (index < arraylength) ? index++ : index = 0;
+          return (
+            <CardItem
+              imageSrc={image.src}
+              title={image.name}
+              key={image.id}
+              fg={foregroundColors[index]}
+              bg={backgroundColors[index]}
+            />
+          )
+        })}
     </GridWrapper>
   );
 }
 
 export default CardGrid;
 
+
+
 const GridWrapper = styled.div`
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
   gap: 3.2rem;
+  transition: all 200ms;
+  width: 1200px;
+  min-width: 600px;
+  margin: auto;
+
+  &:after {
+    width: 1200px;
+    min-width: 600px;
+    content: "";
+    flex: auto;
+  }
+
 `
+
