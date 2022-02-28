bfb93f3590cfb56098844990a95704c789bea4ac
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-02-27 23:27:03 -0800

    Style header

--- memory-game-app/src/components/Header/Header.js
+++ memory-game-app/src/components/Header/Header.js
@@ -3,13 +3,15 @@ import styled from 'styled-components';
 const Header = () => {
   return (
     <HeaderWrapper>
-      <h1>Animal Memory Game</h1>
+      <h1>Memory Game</h1>
     </HeaderWrapper>
   );
 }
 export default Header;
 
 const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
   height: 8rem;
   background: #3B8C47;
 `
