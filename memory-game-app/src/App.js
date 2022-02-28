89f8989685f563b8a1831bee3da2ca965f8f753f
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-02-28 01:24:59 -0800

    Add card clicking handler with console.log test

--- memory-game-app/src/App.js
+++ memory-game-app/src/App.js
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import Gameboard from "./components/Gameboard/Gameboard";
 import GameHeader from "./components/GameHeader/Header";
 
 function App() {
+  const handleCardClicked = (id) => {
+    console.log(`${id} Card Clicked`);
+  }
   return (
     <>
     <GameHeader />
-    <Gameboard />
+    <Gameboard
+      handleCardClickedProp={handleCardClicked} 
+    />
     </>
       
   );
