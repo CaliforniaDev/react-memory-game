2bc2fc2cb8af7fe8312b6f4ad6797c6710fba6d9
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-02 14:04:48 -0800

    import error boundary component

--- memory-game-app/src/index.js
+++ memory-game-app/src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import GlobalStyle from './theme/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme/theme';
+import ErrorBoundary from './utils/ErrorBoundary';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
