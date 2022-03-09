ede156e07406744b6fc6203d88bbbee992e55493
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-08 22:39:11 -0800

    Nest cards within styled component

--- memory-game-app/src/components/Gameboard/Gameboard.js
+++ memory-game-app/src/components/Gameboard/Gameboard.js
@@ -1,32 +1,28 @@
-import React, { useState } from 'react';
 import styled from 'styled-components';
 import CardItem from './Card/CardItem';
-import CardGrid from './Card/CardGrid';
 
 const Gameboard = ({
   handleCardClickedProp,
   cards
 }) => {
-  const [level, setLevel] = useState(1);
-  
   return (
     <MainContainer>
-      {cards && cards.map(card => {
-        console.log(card.bg);
-        return (
-          <CardItem
-            imageSrc={card.src}
-            title={card.name}
-            key={card.id}
-            id={card.id}
-            fg={card.fgColor}
-            bg={card.bgColor}
-            onClick={handleCardClickedProp}
-            draggable="false" 
-          />
-        )
-      })}
-      {/* <CardGrid handleCardClickedProp={handleCardClickedProp} /> */}
+      <BoardWrapper>
+        {cards && cards.map(card => {
+          return (
+            <CardItem
+              imageSrc={card.src}
+              title={card.name}
+              key={card.id}
+              id={card.id}
+              fg={card.fgColor}
+              bg={card.bgColor}
+              onClick={handleCardClickedProp}
+              draggable="false"
+            />
+          )
+        })}
+      </BoardWrapper>
     </MainContainer>
   );
 }
@@ -42,3 +38,22 @@ const MainContainer = styled.main`
   width: 100vw;
   height: 100%;
 `
+const BoardWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 3.2rem;
+  transition: all 200ms;
+  width: 1200px;
+  min-width: 600px;
+  margin: auto;
+
+
+`
+
+  // &:after {
+  //   width: 1200px;
+  //   min-width: 600px;
+  //   content: "";
+  //   flex: auto;
+  // }
