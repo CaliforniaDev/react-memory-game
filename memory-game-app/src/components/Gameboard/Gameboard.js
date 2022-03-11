1c933b6a6b4e73c1e27c9127918a954ba554ba07
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-11 02:07:30 -0800

    Style gameboard container

--- memory-game-app/src/components/Gameboard/Gameboard.js
+++ memory-game-app/src/components/Gameboard/Gameboard.js
@@ -32,28 +32,20 @@ export default Gameboard;
 const MainContainer = styled.main`
   display: flex;
   justify-content: center;
-  padding: 2.4rem;
+  padding: 16rem 2.4rem;
   flex-wrap: wrap;
   background: #403B31;
   width: 100vw;
   height: 100%;
 `
 const BoardWrapper = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 200px); /* 2 */
+  grid-template-rows: auto;
+  grid-gap: 3.2rem; /* 3 */
   justify-content: center;
-  flex-wrap: wrap;
-  gap: 3.2rem;
-  transition: all 200ms;
-  width: 1200px;
-  min-width: 600px;
-  margin: auto;
+  width: 100%;
 
 
 `
 
-  // &:after {
-  //   width: 1200px;
-  //   min-width: 600px;
-  //   content: "";
-  //   flex: auto;
-  // }
