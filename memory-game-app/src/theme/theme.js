64f1e92b1c91404b38eb773be5599160f608929b
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-11 17:09:47 -0800

    Fix key names

--- memory-game-app/src/theme/theme.js
+++ memory-game-app/src/theme/theme.js
@@ -1,13 +1,16 @@
 const theme = {
   fg:{
-    green: '#3B8C47',
-    yellow: '#C4B072',
-    orange: '#F28705',
+    green: '#677D6E',
+    yellow: '#B29856',
+    gray: '#A6A08D',
   },
   bg: {
     green: '#285E30',
     yellow: '#5E5437',
     orange: '#C46E04',
-  }
+  },
+  gameboard: '#5C4C39',
+  header: '#9C4935',
+  lightFont: '#FFFFF0'
 }
 export default theme;
