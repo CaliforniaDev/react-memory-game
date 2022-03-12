5b505edf33cd2b25d745bd7b10172c23a1518130
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-11 17:04:15 -0800

    Import font face

--- memory-game-app/src/theme/GlobalStyle.js
+++ memory-game-app/src/theme/GlobalStyle.js
@@ -1,6 +1,24 @@
 import { createGlobalStyle } from "styled-components";
-
+import CabinRegularW2 from '../lib/assets/fonts/Cabin-Regular.woff2';
+import CabinRegular from '../lib/assets/fonts/Cabin-Regular.woff';
+import RobotoCondensedW2 from '../lib/assets/fonts/Roboto-Regular.woff2';
+import RobotoCondensed from '../lib/assets/fonts/Roboto-Regular.woff';
 const GlobalStyle = createGlobalStyle `
+
+@font-face {
+    font-family: "Cabin";
+    src: url(${ CabinRegularW2 }) format("woff2"),
+         url(${ CabinRegular }) format("woff");
+         font-weight: normal;
+         font-style: normal;
+}
+@font-face {
+    font-family: "Roboto Condensed";
+    src: url(${ RobotoCondensedW2 }) format("woff2"),
+         url(${ RobotoCondensed }) format("woff");
+         font-weight: normal;
+         font-style: normal; 
+}
 
 html {
     font-size: 62.5%;
@@ -19,7 +37,8 @@ html {
     -moz-box-sizing: inherit;
 }
 body, textarea {
-    font-family: 'Nunito Sans';
+    font-family: 'Cabin';
+    letter-spacing: 1px;
     font-size: 1.6rem;
     margin: 0;
 }
@@ -34,6 +53,7 @@ h6 {
 }
 h1 {
     font-size: 5.6rem;
+    font-family: "Roboto Condensed"
 }
 h2 {
     font-size: 4rem;
