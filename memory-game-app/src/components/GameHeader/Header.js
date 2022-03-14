efa894ae875c71c60d3c25bd356204ab60b52c31
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-14 01:33:45 -0700

    Add scroll event handler and state managment

--- memory-game-app/src/components/GameHeader/Header.js
+++ memory-game-app/src/components/GameHeader/Header.js
@@ -1,9 +1,27 @@
 import styled from 'styled-components';
 import Scoreboard from './Scoreboard';
+import { useState, useEffect } from 'react';
 
 const GameHeader = ({ score, bestScore }) => {
+  const [scrollY, setScrollY] = useState(0);
+  const [isActive, setActive] = useState(false);
+
+  useEffect(() => {
+    function watchScroll() {
+      window.addEventListener("scroll", changeOpacity);
+    }
+    watchScroll();
+    return () => {
+      window.removeEventListener("scroll", changeOpacity);
+    }
+  })
+  function changeOpacity() {
+    setScrollY(window.pageYOffset);
+    if (scrollY > 10) return setActive(true);
+    if (scrollY < 10 ) return setActive(false);
+  }
   return (
-    <HeaderSection>
+    <HeaderSection active={isActive}>
       <h1>Memory Game</h1>
       <Scoreboard score={score} bestScore={bestScore} />
     </HeaderSection>
@@ -25,4 +43,6 @@ const HeaderSection = styled.header`
   color: ${({ theme }) => theme.lightFont};
   box-shadow: 0px 1px 10px #333;
   border-radius: 2px;
+  transition: 0.4s;
+  opacity: ${({ active }) => active ? 0.75 : 1};
 `
