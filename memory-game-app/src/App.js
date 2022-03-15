ed89591e38e1ef5777abbbd327a5cc84ea36eb11
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-14 17:11:53 -0700

    Refactor

--- memory-game-app/src/App.js
+++ memory-game-app/src/App.js
@@ -1,30 +1,19 @@
-import { useEffect } from 'react';
-import theme from './theme/theme';
-import { useScore } from './hooks/useScore'
-import { useCards } from './hooks/useCards';
-import { useCardsClicked } from './hooks/useCardsClicked';
+import { useEffect } from "react";
+import theme from "./theme/theme";
+import { useScore } from "./hooks/useScore";
+import { useCards } from "./hooks/useCards";
+import { useCardsClicked } from "./hooks/useCardsClicked";
 import Gameboard from "./components/Gameboard/Gameboard";
 import GameHeader from "./components/GameHeader/Header";
 
 function App() {
-  const [
-    score,
-    bestScore,
-    resetScore,
-    updateScore] = useScore();
-  const [
-    checkCardsClicked,
-    updateCardsClicked,
-    resetCardsClicked] = useCardsClicked();
-  const [
-    cards,
-    updateCards,
-    shuffleCards] = useCards();
+  const [score, bestScore, resetScore, updateScore] = useScore();
+  const [checkCardsClicked, updateCardsClicked, resetCardsClicked] = useCardsClicked();
+  const [cards, updateCards, shuffleCards] = useCards();
 
   useEffect(() => {
     updateCards(theme);
-  }, [cards]) // eslint-disable-line react-hooks/exhaustive-deps
-
+  }, [cards]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const handleCardClicked = (id) => {
     if (checkCardsClicked(id)) {
@@ -34,20 +23,14 @@ function App() {
     shuffleCards();
     updateScore(1);
     return updateCardsClicked(id);
-  }
-
+  };
 
   return (
     <>
       <GameHeader score={score} bestScore={bestScore} />
-      <Gameboard
-        cards={cards}
-        handleCardClickedProp={handleCardClicked}
-      />
+      <Gameboard cards={cards} handleCardClickedProp={handleCardClicked} />
     </>
-
   );
 }
-
 
 export default App;
