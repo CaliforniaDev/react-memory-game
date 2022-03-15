20fe03ea4e25d717b667ba16818505447433c3dc
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-14 17:11:17 -0700

    Refactor

--- memory-game-app/src/theme/theme.js
+++ memory-game-app/src/theme/theme.js
@@ -1,16 +1,16 @@
 const theme = {
-  fg:{
-    green: '#677D6E',
-    yellow: '#B29856',
-    gray: '#A6A08D',
+  fg: {
+    green: "#677D6E",
+    yellow: "#B29856",
+    gray: "#A6A08D",
   },
   bg: {
-    green: '#285E30',
-    yellow: '#5E5437',
-    orange: '#C46E04',
+    green: "#285E30",
+    yellow: "#5E5437",
+    orange: "#C46E04",
   },
-  gameboard: '#5C4C39',
-  header: '#9C4935',
-  lightFont: '#FFFFF0'
-}
+  gameboard: "#5C4C39",
+  header: "#9C4935",
+  lightFont: "#FFFFF0",
+};
 export default theme;
