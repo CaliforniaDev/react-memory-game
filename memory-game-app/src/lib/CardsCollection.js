a15bde74c0b29022ccba512f9ec88fb2fd21dbf6
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-17 02:56:03 -0700

    OVERHAUL add function to retrieve and return specific quantity of random shuffled cards

--- memory-game-app/src/lib/CardsCollection.js
+++ memory-game-app/src/lib/CardsCollection.js
@@ -1,50 +1,66 @@
-import imagesArray from './CardImagesArray';
+import imagesArray from "./CardImagesArray";
 
-const CARDS = imagesArray;
+const CARDS = imagesArray,
+  MAX = CARDS.length - 1;
 
+function generateRandomizedIndex() {
+  const index = Math.floor(Math.random() * MAX) + 1;
+  return index;
+}
 
-function updateForegroundTheme(theme) {
+function getRandomizedImage(quantity) {
+  const cardImages = [];
+  for (let i = 0; i < quantity; i++) {
+    cardImages.push(CARDS[generateRandomizedIndex()]);
+  }
+  return cardImages;
+}
+
+function retrieveForegroundTheme(cards, theme) {
   const foregroundColors = Object.values(theme.fg);
   const arrayLength = foregroundColors.length;
   let index = 0;
-
-  CARDS.map(cardItem => {
-    cardItem.fgColor = foregroundColors[index];
-    (index < arrayLength - 1) ? index++ : index = 0;
-    return cardItem;
-  })
-  return CARDS;
-}
-
-function updateBackgroundTheme(theme) {
-  const backgroundColors = Object.values(theme.bg);
-    const arrayLength = backgroundColors.length;
-    let index = 0;
-    CARDS.map(cardItem => {
-      cardItem.bgColor = backgroundColors[index];
-      (index < arrayLength - 1) ? index++ : index = 0;
+  Promise.all(
+    cards.map((cardItem) => {
+      cardItem.fgColor = foregroundColors[index];
+      if (index < arrayLength - 1) index++;
+      if (index > arrayLength - 1) index = 0;
       return cardItem;
     })
-
-    return CARDS;
+  );
+  return cards;
 }
 
+// const updateBackgroundTheme = async (cards, theme) => {
+//   const backgroundColors = await Object.values(theme.bg);
+//     const arrayLength = backgroundColors.length;
+//     let index = 0;
+//     Promise.all(cards.map(cardItem => {
+//       cardItem.bgColor = backgroundColors[index];
+//       console.log(`Index: ${index}`);
+//       console.log(cardItem.bgColor);
+//       (index < arrayLength - 1) ? index++ : index = 0;
+//       return cardItem;
+//     }));
+//     return cards;
+// }
+
 function cardShuffler(cards) {
-  const shuffled = cards.sort(() => Math.random() - 0.5)
+  const shuffled = cards.sort(() => Math.random() - 0.5);
   return shuffled;
 }
 
-function retrieveCardItems (theme) {
-  const shuffledCards = cardShuffler(CARDS)
-  updateBackgroundTheme(theme);
-  updateForegroundTheme(theme);
+const retrieveCardItems = (quantity, theme) => {
+  const cards = getRandomizedImage(quantity);
+  const shuffledCards = cardShuffler(cards);
+  // await updateBackgroundTheme(cards, theme);
+  retrieveForegroundTheme(cards, theme);
   return shuffledCards;
-}
-
+};
 
 const CardsCollection = {
   retrieveCardItems,
-  cardShuffler
-}
+  cardShuffler,
+};
 
-export {CardsCollection as default};
+export { CardsCollection as default };
