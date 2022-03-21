683501a5f78d8c8ceb252f98cec003ea3e894dd6
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-21 13:15:03 -0700

    OVERHAUL Add useLevel hook, nest update cards handler into higher order function. Refactor if statements, add method which checks if all cards have been clicked

--- memory-game-app/src/App.js
+++ memory-game-app/src/App.js
@@ -3,28 +3,47 @@ import theme from "./theme/theme";
 import { useScore } from "./hooks/useScore";
 import { useCards } from "./hooks/useCards";
 import { useCardsClicked } from "./hooks/useCardsClicked";
+import { useLevel } from "./hooks/useLevel";
 import Gameboard from "./components/Gameboard/Gameboard";
 import GameHeader from "./components/GameHeader/Header";
 
 function App() {
   const [score, bestScore, resetScore, updateScore] = useScore();
-  const [checkCardsClicked, updateCardsClicked, resetCardsClicked] = useCardsClicked();
+  const [
+    checkCardsClicked,
+    updateCardsClicked,
+    isAllCardsClicked,
+    resetCardsClicked,
+  ] = useCardsClicked();
+
   const [cards, updateCards, shuffleCards] = useCards();
-  const quantity = 4;
+  const [level, nextLevel, resetLevel] = useLevel(score);
+
   useEffect(() => {
-    updateCards(quantity, theme);
-  }, [quantity]); // eslint-disable-line react-hooks/exhaustive-deps
+    startLevel();
+  }, [level]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const handleCardClicked = (id) => {
-    if (checkCardsClicked(id)) {
-      resetCardsClicked();
-      return resetScore();
-    }
-    shuffleCards();
-    updateScore(1);
-    return updateCardsClicked(id);
+  const startLevel = async () => {
+    await updateCards(level.cardCount, theme);
   };
+  const handleCardClicked = (id) => {
+    if (!checkCardsClicked(id)) {
+      updateCardsClicked(id);
+      updateScore(1);
 
+      if (isAllCardsClicked(level.cardCount)) {
+        nextLevel();
+        resetCardsClicked();
+      } else {
+        shuffleCards();
+      }
+    } else {
+      resetScore();
+      resetCardsClicked();
+      resetLevel();
+      shuffleCards();
+    }
+  };
   return (
     <>
       <GameHeader score={score} bestScore={bestScore} />
