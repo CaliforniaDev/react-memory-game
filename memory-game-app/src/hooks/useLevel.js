d95eae95219efb64a1099c5b33946c2615f3f62d
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-21 13:16:03 -0700

    Add and export hook which changes game levels

---
+++ memory-game-app/src/hooks/useLevel.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export const useLevel = () => {
+  const CARD_ADDEND = 2,
+    MAX_CARDS = 16;
+  const INITIAL_STATE = { cardCount: 4, currentLevel: 1 };
+  const [level, setLevel] = useState(INITIAL_STATE);
+
+  const nextLevel = () => {
+    setLevel((prevState) => {
+      let cardCount =
+        prevState.cardCount < MAX_CARDS
+          ? prevState.cardCount + CARD_ADDEND
+          : MAX_CARDS;
+      let currentLevel = prevState.currentLevel + 1;
+
+      return { cardCount, currentLevel };
+    });
+  };
+
+  const resetLevel = () => {
+    setLevel({ ...INITIAL_STATE });
+  };
+  return [level, nextLevel, resetLevel];
+};
