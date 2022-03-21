bb3de973df53a20a2f47c84c79a12725559bfe3a
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-21 13:18:05 -0700

    Format

--- memory-game-app/src/hooks/useCards.js
+++ memory-game-app/src/hooks/useCards.js
@@ -1,17 +1,16 @@
-import { useState } from 'react';
-import CardsCollection from '../lib/CardsCollection';
-
+import { useState } from "react";
+import CardsCollection from "../lib/CardsCollection";
 
 export const useCards = () => {
   const [cards, setCards] = useState(null);
 
   const shuffleCards = () => {
     setCards((prevCards) => CardsCollection.cardShuffler(prevCards));
-  }
+  };
 
   const updateCards = async (quantity, theme) => {
     const newCards = await CardsCollection.retrieveCardItems(quantity, theme);
     setCards(newCards);
-  }
+  };
   return [cards, updateCards, shuffleCards];
-}
+};
