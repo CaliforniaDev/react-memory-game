80fbbdad7ad33ae808dbc6aef0448528e8b96621
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-22 17:47:17 -0700

    Extract hooks from parent app component

--- memory-game-app/src/hooks/useGame.js
+++ memory-game-app/src/hooks/useGame.js
@@ -1,3 +1,4 @@
+import theme from "../theme/theme";
 import { useState, useEffect } from "react";
 import { useCards } from "./useCards";
 import { useCardsClicked } from "./useCardsClicked";
@@ -8,6 +9,54 @@ export const useGame = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isGameOver, setIsGameOver] = useState(false);
 
+  const [score, bestScore, resetScore, updateScore] = useScore();
+  const [cards, updateCards, shuffleCards] = useCards();
+  const [level, nextLevel, resetLevel] = useLevel();
+  const [
+    checkCardsClicked,
+    updateCardsClicked,
+    isAllCardsClicked,
+    resetCardsClicked,
+  ] = useCardsClicked();
 
-  return [isLoading];
+  const startLevel = async () => {
+    await updateCards(level.cardCount, theme);
+  };
+
+  function handleCorrectClick(id) {
+    console.log("correctClick");
+    updateCardsClicked(id);
+    updateScore(1);
+    shuffleCards();
+    return;
+  }
+  function handleNextLevel() {
+    nextLevel();
+    resetCardsClicked();
+    shuffleCards();
+    return;
+  }
+  function handleGameOver() {
+    console.log("gameOver");
+    resetScore();
+    resetCardsClicked();
+    resetLevel();
+    shuffleCards();
+    return;
+  }
+
+  function handleCardsClicked(id) {
+    !checkCardsClicked(id) ? handleCorrectClick(id) : handleGameOver();
+    if (isAllCardsClicked(level.cardCount)) return handleNextLevel();
+  }
+
+  return [
+    isLoading,
+    cards,
+    score,
+    bestScore,
+    level,
+    startLevel,
+    handleCardsClicked,
+  ];
 };
