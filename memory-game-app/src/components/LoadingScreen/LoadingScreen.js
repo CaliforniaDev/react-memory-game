f8671189d5548eda9e09fbb931931f2178302fa6
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-22 22:54:28 -0700

    Add loading screen modal

---
+++ memory-game-app/src/components/LoadingScreen/LoadingScreen.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import styled from "styled-components";
+
+const LoadingScreen = () => {
+
+  return (
+    <LoadingContainer>
+
+    </LoadingContainer>
+  )
+
+  const LoadingContainer = styled.div`
+  
+  `
+}
