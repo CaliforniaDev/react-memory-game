c1cb804c2ea79a7ca276ee22d827e619aac4c962
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-23 17:12:31 -0700

    Style loading screen

--- memory-game-app/src/components/LoadingScreen/LoadingScreen.js
+++ memory-game-app/src/components/LoadingScreen/LoadingScreen.js
@@ -1,15 +1,29 @@
 import { useEffect } from "react";
 import styled from "styled-components";
+import icon from "../../lib/assets/Cube-1s-200px.gif";
 
-const LoadingScreen = () => {
-
+const LoadingScreen = ({ show }) => {
   return (
-    <LoadingContainer>
+    show && (
+      <LoadingContainer>
+        <img src={icon} alt="Loading..." />
+      </LoadingContainer>
+    )
+  );
 
-    </LoadingContainer>
-  )
 
-  const LoadingContainer = styled.div`
-  
-  `
-}
+};
+
+export default LoadingScreen;
+
+const LoadingContainer = styled.div`
+min-width: 100vw;
+min-height: 100vh;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+position: absolute;
+background: green;
+z-index: 2000;
+`;
