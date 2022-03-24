950307b70eaaa5455bab89479e320f2138cb4aee
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-24 16:01:04 -0700

    Change size object

--- memory-game-app/src/components/device.js
+++ memory-game-app/src/components/device.js
@@ -1,8 +1,8 @@
 const size = {
-  mobileS: "320px",
-  mobileM: "375px",
-  mobileL: "425px",
-  tablet: "768px",
+  mobileS: "375px",
+  mobileM: "425px",
+  mobileL: "688px",
+  tablet: "992px",
   laptop: "1024px",
   laptopL: "1440px",
   desktop: "2560px",
