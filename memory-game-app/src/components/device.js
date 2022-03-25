45b574c43d4841402778876831d97ed6b1558b47
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-24 17:01:19 -0700

    Add max width object

--- memory-game-app/src/components/device.js
+++ memory-game-app/src/components/device.js
@@ -8,7 +8,7 @@ const size = {
   desktop: "2560px",
 };
 
-export const device = {
+export const deviceMinWidth = {
   mobileS: `(min-width: ${size.mobileS})`,
   mobileM: `(min-width: ${size.mobileM})`,
   mobileL: `(min-width: ${size.mobileL})`,
@@ -16,4 +16,14 @@ export const device = {
   laptop: `(min-width: ${size.laptop})`,
   laptopL: `(min-width: ${size.laptopL})`,
   desktop: `(min-width: ${size.desktop})`,
+};
+
+export const deviceMaxWidth = {
+  mobileS: `(max-width: ${size.mobileS})`,
+  mobileM: `(max-width: ${size.mobileM})`,
+  mobileL: `(max-width: ${size.mobileL})`,
+  tablet: `(max-width: ${size.tablet})`,
+  laptop: `(max-width: ${size.laptop})`,
+  laptopL: `(max-width: ${size.laptopL})`,
+  desktop: `(max-width: ${size.desktop})`,
 };
